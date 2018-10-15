16810f43d514590b528e574bc73e594182826efe
Stilwell, Thomas <Thomas.Stilwell@nordicsemi.no>
2018-10-15 02:07:13 +0200

    customize docker run script for ubuntu

--- docker-run.sh
+++ docker-run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-docker run -d --name="Zoneminder" \
---net="bridge" \
---privileged="true" \
--p 8080:80/tcp \
--p 8443:443/tcp \
--p 9000:9000/tcp \
--e TZ="America/New_York" \
--e SHMEM="50%" \
--e SSL_EVENTS="1" \
--e PUID="99" \
--e PGID="100" \
--v "/mnt/cache/appdata/Zoneminder":"/config":rw \
--v "/mnt/cache/appdata/Zoneminder/data":"/var/cache/zoneminder":rw \
-zoneminder
+docker run -d --name="ZM" \
+		--net="bridge" \
+		--privileged="true" \
+		-p 80:80/tcp \
+		-p 8443:443/tcp \
+		-p 9000:9000/tcp \
+		-e TZ="Europe/Oslo" \
+		-e SHMEM="50%" \
+		-e SSL_EVENTS="1" \
+		-e PGID=$(id -g $USER) -e PUID=$(id -u $USER) \		
+		-v "/mnt/SS1/zm":"/config":rw \
+		-v "/mnt/SS1/zm/data":"/var/cache/zoneminder":rw \
+	zoneminder
+
+		# -e PUID="99" \
+		# -e PGID="100" \
